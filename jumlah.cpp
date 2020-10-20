e9c4de1831353e7c98bd3bd1978c21cc75919881
Mohamad Nurul Ihsan <44545824+ikhsanlosiver@users.noreply.github.com>
2020-10-20 16:07:05 +0700

    Create jumlah.cpp

---
+++ jumlah.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int a,b,c;
+
+cout<<"masukkan nilai a :;
+cin>>a;
+cout<<endl;
+cout<<"masukkan nilai b :;
+cin>>b;
+cout<<endl;
+c= a+b;
+cout<<HASIL =<<c<<endl;
+
+}
