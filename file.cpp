cfc1173d2526db3c0a822752370fc7f46b4f9441
alfan suffa pramana <57166161+alfansuffa05@users.noreply.github.com>
2020-10-23 23:47:39 +0800

    Create file.cpp
    project file for c++ code language

---
+++ file.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+ int main()
+ {
+char nama_mk [20];
+char kode_mk[13];
+int ulang;
+ofstream matkul;
+matkul.open("matkul.txt",ios::in);
+cout<<"input jumlah mata kuliah :";
+cin>>ulang;
+for (int i=1; i<=ulang; i++) 
+{
+cout<<"Nama Mata Kuliah   : ";
+cin>>nama_mk;
+cout<<"Kode Mata Kuliah   : ";
+cin>>kode_mk;
+matkul<<"NAMA MATA KULIAH       :"<<nama_mk<<endl;
+matkul<<"KODE MATA KULIAH       :"<<kode_mk<<endl; 
+}
+matkul.close();
+}
+
+
